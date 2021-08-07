d49e1956a2dde5a9c46010ea38d5a68336ea86a2
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-08-07 16:08:00 +0200

    init:complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
+    const createArr = String(n).split('')
+
+    createArr.includes('-')?createArr.shift():createArr
   
+    return createArr.reverse().join('')
 }
